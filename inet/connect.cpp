74dc33e84054da0fc2e3cb264042b94a84403514
NathanNuckels <48299408+NathanNuckels@users.noreply.github.com>
2023-01-20 08:58:17 -0600

    Update connect.cpp

--- inet/connect.cpp
+++ inet/connect.cpp
@@ -1,3 +1,4 @@
+#include<iostream>
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netdb.h>
@@ -16,7 +17,7 @@ namespace inet{
             return -1;
         }
         
-        int sock = socket(res->family,res->ai_socktype,res->ai_protocol);
+        int sock = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
         if(sock==-1){
             std::cerr<<strerror(errno)<<std::endl;
             freeaddrinfo(res);
@@ -38,19 +39,19 @@ namespace inet{
         memset(&hints,0,sizeof(hints));
         hints.ai_family=AF_UNSPEC;
         hints.ai_socktype=SOCK_STREAM;
-        hints.ai_flag=AI_PASSIVE;
+        hints.ai_flags=AI_PASSIVE;
         int status = getaddrinfo(NULL,protocol,&hints,&res);
         if(status!=0){
             std::cerr<<gai_strerror(status)<<std::endl;
             return -1;
         }
-        int sock = socket(res->family,res->ai_socktype,res->ai_protocol);
+        int sock = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
         if(sock==-1){
             std::cerr<<strerror(errno)<<std::endl;
             freeaddrinfo(res);
             return -1;
         }
-        status = bind(sock,res->ai_addr,res->ai_addrlen;);
+        status = bind(sock,res->ai_addr,res->ai_addrlen);
         if(status==-1){
             freeaddrinfo(res);
             std::cerr<<strerror(errno)<<std::endl;
